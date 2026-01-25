3d0a81912ca1e751e08d2a125d7013fac927d53f
amvern <drkmannn@gmail.com>
2026-01-25 01:11:49 -0800

    feat: block placement guide ghost preview

--- src/client/java/github/amvern/twodimensionalreloaded/client/config/ClientConfig.java
+++ src/client/java/github/amvern/twodimensionalreloaded/client/config/ClientConfig.java
@@ -7,6 +7,13 @@ import me.shedaniel.autoconfig.annotation.Config;
 @Config(name = TwoDimensionalReloaded.MOD_ID)
 public class ClientConfig implements ConfigData {
     public boolean renderBlockPlacementGuide = false;
+    public boolean renderPlacementOutline = true;
+    public static RenderStyle blockRenderMode = RenderStyle.GHOST_BLOCK;
+
+    public enum RenderStyle {
+        FULL_BLOCK,
+        GHOST_BLOCK
+    }
 
     public int placeableOutlineColor = 0x8000FF00;
     public int nonPlaceableOutlineColor = 0x80FF0000;
--- src/client/java/github/amvern/twodimensionalreloaded/client/config/ClientConfigScreen.java
+++ src/client/java/github/amvern/twodimensionalreloaded/client/config/ClientConfigScreen.java
@@ -10,45 +10,59 @@ public class ClientConfigScreen {
 
     public static Screen create(Screen parent, ClientConfig config) {
         ConfigBuilder builder = ConfigBuilder.create()
-                .setParentScreen(parent)
-                .setTitle(Component.literal("Two Dimensional: Reloaded Options"));
+            .setParentScreen(parent)
+            .setTitle(Component.literal("Two Dimensional: Reloaded Options"));
 
         ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
         ConfigCategory placementOutlineCategory = builder.getOrCreateCategory(Component.literal("Placement Outline"));
 
         placementOutlineCategory.addEntry(entryBuilder.startTextDescription(
-                Component.literal("Block Placement Guide Options. Defaults to 32-bit ARGB color. Hex colors like #RRGGBB will automatically become opaque.")
+            Component.literal("Block Placement Guide Options. Defaults to 32-bit ARGB color. Hex colors like #RRGGBB will automatically become opaque.")
         ).build());
 
         placementOutlineCategory.addEntry(entryBuilder.startBooleanToggle(Component.literal("Render Block Placement Guide"), config.renderBlockPlacementGuide)
-                .setDefaultValue(false)
-                .setSaveConsumer(value -> config.renderBlockPlacementGuide = value)
-                .build()
+            .setDefaultValue(false)
+            .setSaveConsumer(value -> config.renderBlockPlacementGuide = value)
+            .setTooltip(Component.literal("Render placement preview"))
+            .build()
+        );
+
+        placementOutlineCategory.addEntry(entryBuilder.startEnumSelector(Component.literal("Placement Preview Style"), ClientConfig.RenderStyle.class, ClientConfig.blockRenderMode)
+            .setDefaultValue(ClientConfig.RenderStyle.GHOST_BLOCK)
+            .setSaveConsumer(value -> config.blockRenderMode = value)
+            .setEnumNameProvider(style -> Component.nullToEmpty(style.name().replace("_", " ")))
+            .build()
+        );
+
+        placementOutlineCategory.addEntry(entryBuilder.startBooleanToggle(Component.literal("Show Outline"), config.renderPlacementOutline)
+            .setDefaultValue(true)
+            .setSaveConsumer(value -> config.renderPlacementOutline = value)
+            .setTooltip(Component.literal("Render outline around placement preview"))
+            .build()
         );
 
         placementOutlineCategory.addEntry(entryBuilder.startColorField(Component.literal("Placeable Outline"), config.placeableOutlineColor)
-                .setDefaultValue(0x8000FF00)
-                .setAlphaMode(true)
-                .setSaveConsumer(value -> config.placeableOutlineColor = fixAlpha(value))
-                .build()
+            .setDefaultValue(0x8000FF00)
+            .setAlphaMode(true)
+            .setSaveConsumer(value -> config.placeableOutlineColor = fixAlpha(value))
+            .build()
         );
 
         placementOutlineCategory.addEntry(entryBuilder.startColorField(Component.literal("Non-Placeable Outline"), config.nonPlaceableOutlineColor)
-                .setDefaultValue(0x80FF0000)
-                .setAlphaMode(true)
-                .setSaveConsumer(value -> config.nonPlaceableOutlineColor = fixAlpha(value))
-                .build()
+            .setDefaultValue(0x80FF0000)
+            .setAlphaMode(true)
+            .setSaveConsumer(value -> config.nonPlaceableOutlineColor = fixAlpha(value))
+            .build()
         );
 
-        placementOutlineCategory.addEntry(entryBuilder.startTextDescription(
-                Component.literal("Toggle Fog Environments for Water/Lava/Powerdered Snow.")
-        ).build());
+        placementOutlineCategory.addEntry(entryBuilder.startTextDescription(Component.literal("Toggle Fog Environments for Water/Lava/Powerdered Snow."))
+            .build());
 
         placementOutlineCategory.addEntry(entryBuilder.startBooleanToggle(Component.literal("Render Fog"), config.renderFogEnvironments)
-                .setDefaultValue(true)
-                .setSaveConsumer(value -> config.renderFogEnvironments = value)
-                .build()
+            .setDefaultValue(true)
+            .setSaveConsumer(value -> config.renderFogEnvironments = value)
+            .build()
         );
 
         return builder.build();
